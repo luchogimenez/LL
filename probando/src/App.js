d02440d742c62600d3571ccee851c0ff206b8e59
Newvilla97 <villanuevaleo@outlook.com>
2020-07-25 22:19:36 -0300

    agregando otro titulo

--- probando/src/App.js
+++ probando/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido Luis
+          Bienvenido Luis y leo
         </p>
         <a
           className="App-link"
